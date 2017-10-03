4eab047e7a42d067ea2d0e16e66d8e04946d6abc
Samuel Potter <potters5@coventry.ac.uk>
2017-10-03 10:50:12 +0100

    Added config definitions

--- 301CR-Core/Main.cpp
+++ 301CR-Core/Main.cpp
@@ -1,8 +1,16 @@
 #include "Logger.h"
 
 
+#ifdef BUILD_DEBUG
 int main(char** argv, int argc)
 {
 	LOG_ERROR("Dog %s", "HELOO");
 	return 0;
 }
+#else
+int main(char** argv, int argc)
+{
+	LOG_ERROR("Dog %s", "HELOO");
+	return 0;
+}
+#endif
